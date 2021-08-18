c32db59541882a7b16a4174579e1a8b5f59586d1
Alan Almeida <alan241015@gmail.com>
2021-08-19 02:15:59 +0530

    add template

--- taskmaster/views.py
+++ taskmaster/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
-from taskmaster import app
+from taskmaster import app, db
 
 @app.route('/')
 def index():
-    return "Hello"
+    return render_template('index.html')
