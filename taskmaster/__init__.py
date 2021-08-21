292c7a10627db37187e743bfdab29e167b667e7b
Alan Almeida <alan241015@gmail.com>
2021-08-22 00:05:11 +0530

    update git ignore

--- taskmaster/__init__.py
+++ taskmaster/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///main.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SECRET_KEY"] = SECRET_KEY
 db = SQLAlchemy(app)
 
 from taskmaster import views, models
