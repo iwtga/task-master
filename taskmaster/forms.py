6a83ca12622eba8087865a5d198f1114b514b2dd
Alan Almeida <alan241015@gmail.com>
2021-08-23 21:15:54 +0530

    modified template

--- taskmaster/forms.py
+++ taskmaster/forms.py
@@ -1,15 +1,16 @@
-from flask.app import Flask
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, PasswordField
-from wtforms.validators import InputRequired, Email, Length
+from wtforms import StringField, BooleanField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Email, Length, EqualTo
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired(), Length(min=4, max=20)])
     password = PasswordField("Password", validators=[InputRequired(), Length(min=8, max=80)])
     remember = BooleanField("Remember Me")
+    submit = SubmitField("Login")
 
 class SignupForm(FlaskForm):
-    email = StringField("Email", validators=[InputRequired(), Length(min=4, max=20)])
+    email = StringField("Email", validators=[InputRequired(), Length(min=4, max=20), Email(message="Invalid Mail")])
     username = StringField("Username", validators=[InputRequired(), Length(min=4, max=20)])
     password = PasswordField("Password", validators=[InputRequired(), Length(min=8, max=80)])
-    confirm_password = PasswordField("Confirm Password", validators=[InputRequired(), Length(min=8, max=80)])
+    confirm_password = PasswordField("Confirm Password", validators=[InputRequired(), Length(min=8, max=80), EqualTo('password')])
+    submit = SubmitField("Sign Up")
